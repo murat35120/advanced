4b567ddf579f3604ceed5031312ca3c1abcf2098
Murat351 <murat351@narod.ru>
2022-12-02 12:14:10 +0400

    parse licenses list

--- tcp socket/server/index.js
+++ tcp socket/server/index.js
@@ -10,7 +10,7 @@ let converters = [];
 
 server.on('connection', function(sock) {
 	//let id=num++;
-	let obj={socket:sock, cmd:commands.new_sock.start};
+	let obj={socket:sock, cmd:commands.new_sock.short_info};
 	//sockets[id]=converter;
 	console.log('CONNECTED: ' + obj.socket.remoteAddress + ':' + obj.socket.remotePort);
 	//sockets[id].socket=sock;
@@ -52,32 +52,70 @@ server.on('connection', function(sock) {
 let commands={
 	new_sock:{
 		start(obj){
+			obj.cmd=commands.new_sock.short_info;
 			let lnk=obj;
-			let timerId = setTimeout(tmr, 100);	
+			let timerId = setTimeout(tmr, 100);
 			function tmr(){
-				commands.new_sock.full_info(lnk);
+				commands.new_sock.tmr(lnk);
 			}
-			
+		},
+		tmr(obj){
+			obj.cmd=commands.new_sock.full_info;
+			commands.full_info(obj);
 		},
 		full_info(obj){
+			obj.full_info=String(obj.data);
 			obj.cmd=commands.new_sock.short_info;
-			commands.full_info(obj);	
+			commands.short_info(obj);		
 		},
 		short_info(obj){
-			obj.cmd=commands.new_sock.end;
-			commands.short_info(obj);	
+			if(obj.data.length){
+				let arr = String(obj.data).split(' ');
+				obj.model=arr[0];
+				if(arr[1].length){
+					console.log(arr[1]);
+					let arr1=arr[1].split(',');
+					if(arr1[0]){
+						obj.number=arr1[0].split(':')[1];
+					}
+					if(arr1[1]){
+						obj.mode=arr1[1].split(':')[1];
+					}
+					if(arr1[2]){
+						obj.key=arr1[2].split('\r')[0];
+					}
+				}
+			}
+			obj.cmd=commands.new_sock.license_list;
+			commands.license_list(obj);		
 		},
-		end(obj){
+		license_list(obj){
+			let arr = String(obj.data).split('LIC: ');
+			let lic={};
+			if(arr.length){
+				for (i=0; i<arr.length; i++){
+					if(arr[i].length){
+						let ss=arr[i].split(' ');
+						if(Number(ss[0])){
+							lic.type=Number(ss[0]);
+							lic.controllers=ss[1].split('/')[0].split('(')[1];
+							lic.cards=ss[1].split('/')[1].split(')')[0];
+							console.log(lic);
+						}
+					}
+				}
+			}
+			obj.lic=lic;
 			console.log('end');
 		}
 	},
 	answer(data, obj){
-		console.log('from : ' + data);
+		console.log(String(data));
 		obj.data=data;
 		obj.cmd(obj);
 	},
 	license_list(obj){
-		let cmd_buf=Buffer.from([0xC8, 0x0D]);//список лицензий
+		let cmd_buf=Buffer.from([0x4C, 0x0D]);//список лицензий
 		obj.socket.write(cmd_buf);	
 	},
 	full_info(obj){
@@ -85,12 +123,10 @@ let commands={
 		obj.socket.write(cmd_buf);
 	},
 	short_info(obj){
-		let cmd_buf=Buffer.from([0x4C, 0x0D]);//краткое описание
+		let cmd_buf=Buffer.from([0xC8, 0x0D]);//краткое описание
 		obj.socket.write(cmd_buf);
 	},
-	base(data, obj, func){
-		sockets[0].socket.write(data.cmd_buf);
-	}
+	
 }
 
 
